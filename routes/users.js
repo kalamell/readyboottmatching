3d384e0e862fcc91afe9f0fc9bf70760e523c041
sankhumpha <sankhumpha84@gmail.com>
2019-08-05 06:54:50 +0700

    update upload

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users');
+const multer = require('multer');
 
 const Users = require('../models/users');
 const Provinces = require('../models/provinces');
-
+const path = require('path');
+const fs = require('fs');
 
 const { isAuth } = require('../helpers/auth');
-
 
 router.get('/', isAuth, async (req, res) => {
 
@@ -39,12 +40,46 @@ router.get('/', isAuth, async (req, res) => {
    
 });
 
+router.get('/edit', isAuth, async (req, res) => {
+
+    const user = req.session.passport.user;
+    const data  = await Users.findOne({facebookid: user.id});
+    const provinces = await Provinces.find({});
+    let data_provinces = [];
+    provinces.forEach(function(e, v) {
+        if (e.province_name == data.province) {
+            select = 'selected';
+        } else { 
+            select = '';
+        }
+
+        data_provinces.push({
+            province_name: e.province_name,
+            select
+        });
+    })
+    res.render('profile', {
+        data,
+        data_provinces
+    });
+   
+});
+
 
 router.get('/update', isAuth, (req, res) => { 
     res.redirect('/user');
 })
 
-router.post('/update', isAuth, async (req, res) => {
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/upload')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
+const upload = multer({ storage: storage });
+router.post('/update', [isAuth, upload.single('file')], async (req, res) => {
     let { sex, interest, province, age, range_min, range_max } = req.body;
     const user = req.session.passport.user;
     await Users.findOneAndUpdate({facebookid: user.id}, {
@@ -55,8 +90,13 @@ router.post('/update', isAuth, async (req, res) => {
        range_min,
        range_max
     });
-    
-    res.redirect('/match');
+
+   if (req.file != null) {
+    await Users.findOneAndUpdate({facebookid: user.id}, {
+       'profile_url': process.env.APP_URL + '/upload/' + req.file.filename
+     });
+   }
+   res.redirect('/match');
    
 })
 
